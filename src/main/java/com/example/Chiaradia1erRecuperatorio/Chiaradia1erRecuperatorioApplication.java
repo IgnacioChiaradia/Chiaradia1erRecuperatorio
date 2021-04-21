a5023b8cfa48812ef477667242cd3ac8ce89c442
ignacio chiaradia <ignachy01@gmail.com>
2021-04-21 17:59:37 -0300

    first commit

---
+++ src/main/java/com/example/Chiaradia1erRecuperatorio/Chiaradia1erRecuperatorioApplication.java
@@ -0,0 +1,13 @@
+package com.example.Chiaradia1erRecuperatorio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chiaradia1erRecuperatorioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chiaradia1erRecuperatorioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Chiaradia1erRecuperatorio/Chiaradia1erRecuperatorioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Chiaradia1erRecuperatorio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chiaradia1erRecuperatorioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
